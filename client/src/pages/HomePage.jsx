7d30e2bde0c51c14a8179e5a85d480f633d635c6
Krit <titatit.akkh@gmail.com>
2024-01-18 15:37:56 +0700

    Finished HH 2nd part

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { useAuth } from "../contexts/authentication";
 import usePosts from "../hooks/usePosts";
 import getPublishedDate from "../utils/getPublishedDate";
 
@@ -15,6 +15,7 @@ function HomePage() {
   useEffect(() => {
     getPosts({ status, keywords, page });
   }, [status, keywords, page]);
+  const { logout } = useAuth();
 
   return (
     <div>
@@ -31,7 +32,13 @@ function HomePage() {
            // 🐨 Todo: Exercise #7
           //  นำ Function `logout` จาก AuthContext มา Execute ใน Prop `onClick`
         */}
-        <button>Logout</button>
+        <button
+          onClick={() => {
+            logout();
+          }}
+        >
+          Logout
+        </button>
       </div>
       <div className="search-box-container">
         <div className="search-box">
